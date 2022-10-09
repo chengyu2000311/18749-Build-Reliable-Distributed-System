0c7fc0784aafb2cc3e62275479fe291caa5f3fbc
chengyu2000311 <chengyu2000311@gmail.com>
2022-10-09 01:53:51 -0400

    adapt for java 8 in ece machine

--- src/util/IpPort.java
+++ src/util/IpPort.java
@@ -1,4 +1,19 @@
 package util;
 
-public record IpPort(String IPAddress, int portNumber) {
+public class IpPort {
+    private String IPAddress;
+    private int portNumber;
+
+    public IpPort(String IPAddress, int portNumber) {
+        this.IPAddress = IPAddress;
+        this.portNumber = portNumber;
+    }
+
+    public String IPAddress() {
+        return IPAddress;
+    }
+
+    public int portNumber() {
+        return portNumber;
+    }
 }
